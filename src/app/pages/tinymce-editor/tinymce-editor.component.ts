cceb10a66448ac23b51438ab61c07c9b349c99f3
李豪珣 <519367854@qq.com>
2021-07-30 15:18:20 +0800

    修改README

--- src/app/pages/tinymce-editor/tinymce-editor.component.ts
+++ src/app/pages/tinymce-editor/tinymce-editor.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {TinyMceConfig} from './tinymce.config';
 
+declare const tinymce;
+
 @Component({
   selector: 'app-tinymce-editor',
   templateUrl: './tinymce-editor.component.html',
@@ -17,11 +19,12 @@ export class TinymceEditorComponent implements OnInit {
   editorConfig = TinyMceConfig;
 
   ngOnInit(): void {
+    console.log(tinymce);
   }
 
 
   editorClick($event){
-    console.log($event);
+    // console.log($event);
   }
 
 }
--- src/app/pages/tinymce-editor/tinymce.config.ts
+++ src/app/pages/tinymce-editor/tinymce.config.ts
@@ -1,36 +1,18 @@
 import {Editor} from 'tinymce';
 
-export const TinyMceConfig = {
-    // inline: true,
-    // skin: 'oxide-dark',
-    base_url: 'assets/tinymce',
-    height: 200,
-    language: 'zh_CN',
-    language_url: 'assets/tinymce/lang/zh_CN.js',
-    branding: false,
-    menubar: false,
-    toolbar: [
-        'undo redo | fontselect fontsizeselect | forecolor backcolor | bold italic | underline strikethrough | alignleft aligncenter | alignright alignjustify | alignnone table | styleselect | 插入 BoldTest'
-    ],
-    placeholder: '请输入内容',
 
-    setup: editorSetup
-      
-}; 
-
-
-function editorSetup(editor: Editor){
+const editorSetup = (editor: Editor) => {
     insertDimension(editor);
     boldTest(editor);
 }
 
-function insertDimension(editor: Editor){
+const insertDimension = (editor: Editor) => {
     editor.ui.registry.addMenuButton('插入', {
         tooltip: '插入',
         text: '插入',
         fetch: (callback) => {
 
-            const template = (text: string) => `<span contenteditable='false' style='color: green;cursor: text;'>[${text}]</span><span contenteditable="false">&ZeroWidthSpace;</span>`;
+            const template = (text: string) => `<span contenteditable='false' style='color: green;cursor: text;font-weight:normal'>[${text}]</span><span contenteditable="false">&ZeroWidthSpace;</span>`;
             
             const items = [
                 {
@@ -54,21 +36,43 @@ function insertDimension(editor: Editor){
 
 }
 
-function boldTest(editor: Editor){
+const boldTest = (editor: Editor) => {
     editor.ui.registry.addButton('BoldTest',{
         tooltip: 'BoldTest',
         text: 'BoldTest',
         onAction: (api) => {
-            console.log(api);
-            console.log(editor.selection);
-
             const content = editor.selection.getContent();
             const node = editor.selection.getNode();
             console.log('node>>>',node);
             console.log('content>>>',content);
             const template = `<strong>${content}</strong>`;
             editor.selection.setContent(template);
+
+            // editor.execCommand('bold');
         }
     });
 
 }
+
+const TinyMceConfig = {
+    // inline: true,
+    // skin: 'oxide-dark',
+    base_url: 'assets/tinymce',
+    height: 200,
+    language: 'zh_CN',
+    language_url: 'assets/tinymce/lang/zh_CN.js',
+    branding: false,
+    menubar: false,
+    toolbar: [
+        'undo redo | fontselect fontsizeselect | forecolor backcolor | bold italic | underline strikethrough | alignleft aligncenter | alignright alignjustify | alignnone table | styleselect | 插入 BoldTest'
+    ],
+    placeholder: '请输入内容',
+    statusbar: false,
+    setup: editorSetup
+      
+}; 
+
+
+
+
+export {TinyMceConfig};
